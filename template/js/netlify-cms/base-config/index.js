26aff76afdd42240fac472207bba3ee48174eb8c
Eduardo <edu.vlemes@gmail.com>
2024-08-23 15:32:07 -0300

    .

--- template/js/netlify-cms/base-config/index.js
+++ template/js/netlify-cms/base-config/index.js
@@ -510,9 +510,9 @@ export default options => {
           widget: 'select',
           default: 3,
           options: [
-            {label:"4", value:3},
-            {label:"3", value:4},
-            {label:"2", value:6},
+            {label:"4", value:4},
+            {label:"3", value:3},
+            {label:"2", value:2},
           ]
         },     
         {
@@ -522,8 +522,8 @@ export default options => {
           widget: 'select',
           default: 12,
           options: [
-            {label:"2", value:6},
-            {label:"1", value:12},
+            {label:"2", value:2},
+            {label:"1", value:1},
           ]
         },    
         {
