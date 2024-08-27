7340df09cecd1dd0afb18bbd0e80cbc22e21b8fd
Eduardo <edu.vlemes@gmail.com>
2024-08-26 22:53:15 -0300

    .

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,5 @@
 // Add your custom JavaScript for storefront pages here.
+document.addEventListener("DOMContentLoaded", function() {
+  let h = $(`header.header`).innerHeight();
+  $(`body`).css(`--header-height`, h + 'px')
+});
