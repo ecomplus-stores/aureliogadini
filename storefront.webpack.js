bb314d8e73fa2175e2a9748eec6377a259a53c51
Eduardo <edu.vlemes@gmail.com>
2024-08-27 23:08:38 -0300

    .

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -4,6 +4,8 @@ module.exports = () => ({
   resolve: {
     alias: {
       './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config'),
+      './html/SearchEngine.html': path.resolve(__dirname, 'template/js/custom-js/html/SearchEngine.html'),   
+      //'./js/SearchEngine.js': path.resolve(__dirname, 'template/js/custom-js/html/SearchEngine.js'),   
     }    
   }
 })
--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,5 +1,19 @@
 // Add your custom JavaScript for storefront pages here.
 document.addEventListener("DOMContentLoaded", function() {
   let h = $(`header.header`).innerHeight();
-  $(`body`).css(`--header-height`, h + 'px')
+  $(`body`).css(`--header-height`, h + 'px');
+
+  if($(`.page--categories`)){
+    $(`.page-title, .page-title + .category-description`).wrapAll(`<div class=category-text></div>`);
+    $(`.category-text, .category-text +  .category-banner`).wrapAll(`<div class=category-brick></div>`)
+  }
+
+  $(`body`).on(`click`,`.listType button`, function(){
+    let attr = $(this).attr(`col`);
+    $(`.listType button`).removeClass(`active`);
+    $(this).closest(`#search-engine`).find(`.product-item`).closest(`div`).removeClass(`col-lg-3`).removeClass(`col-lg-4`).addClass(`col-lg-` + attr)
+    $(this).addClass(`active`)
+
+  })
 });
+
